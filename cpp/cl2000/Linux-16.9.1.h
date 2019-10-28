2e641a51a68426d0ddca13a25d45a6772ccb8a7a
Evan Nemerson <evan@nemerson.com>
2019-10-27 18:56:57 -0700

    Add a few more TI old compilers.

---
+++ cpp/cl2000/Linux-16.9.1.h
@@ -0,0 +1,38 @@
+#define __signed_chars__ 1	/* Predefined */
+#define __PRAGMA_REDEFINE_EXTNAME 1	/* Predefined */
+#define __DATE__ "Oct 27 2019"	/* Predefined */
+#define __TIME__ "18:56:16"	/* Predefined */
+#define __STDC__ 1	/* Predefined */
+#define __cplusplus 199711L	/* Predefined */
+#define _WCHAR_T 1	/* Predefined */
+#define _BOOL 1	/* Predefined */
+#define __ARRAY_OPERATORS 1	/* Predefined */
+#define __PLACEMENT_DELETE 1	/* Predefined */
+#define __EDG_RUNTIME_USES_NAMESPACES 1	/* Predefined */
+#define __TI_C99_COMPLEX_ENABLED__ 1	/* Predefined */
+#define __EDG_TYPE_TRAITS_ENABLED 1	/* Predefined */
+#define __edg_front_end__ 1	/* Predefined */
+#define __EDG_VERSION__ 404	/* Predefined */
+#define __EDG_SIZE_TYPE__ unsigned long	/* Predefined */
+#define __EDG_PTRDIFF_TYPE__ long	/* Predefined */
+#define __GNUC_MINOR__ 3	/* Predefined */
+#define __GNUC_PATCHLEVEL__ 0	/* Predefined */
+#define __VERSION__ "EDG g++ 4.3 mode"	/* Predefined */
+#define __TI_COMPILER_VERSION__ 16009010	/* Predefined */
+#define __COMPILER_VERSION__ 16009010	/* Predefined */
+#define __TMS320C2000__ 1	/* Predefined */
+#define _TMS320C2000 1	/* Predefined */
+#define __TMS320C28XX__ 1	/* Predefined */
+#define _TMS320C28XX 1	/* Predefined */
+#define __TMS320C28X__ 1	/* Predefined */
+#define _TMS320C28X 1	/* Predefined */
+#define __LARGE_MODEL__ 1	/* Predefined */
+#define __SIZE_T_TYPE__ unsigned long	/* Predefined */
+#define __PTRDIFF_T_TYPE__ long	/* Predefined */
+#define __WCHAR_T_TYPE__ unsigned int	/* Predefined */
+#define __little_endian__ 1	/* Predefined */
+#define __TI_STRICT_ANSI_MODE__ 0	/* Predefined */
+#define __TI_WCHAR_T_BITS__ 16	/* Predefined */
+#define __TI_GNU_ATTRIBUTE_SUPPORT__ 1	/* Predefined */
+#define __TI_STRICT_FP_MODE__ 1	/* Predefined */
+#define _OPTIMIZE_FOR_SPACE 1
---
+++ cpp/cl6x/Linux-8.1.7.h
@@ -0,0 +1,42 @@
+#define __signed_chars__ 1	/* Predefined */
+#define __DATE__ "Oct 27 2019"	/* Predefined */
+#define __TIME__ "18:56:16"	/* Predefined */
+#define __STDC__ 1	/* Predefined */
+#define __cplusplus 199711L	/* Predefined */
+#define _WCHAR_T 1	/* Predefined */
+#define _BOOL 1	/* Predefined */
+#define __ARRAY_OPERATORS 1	/* Predefined */
+#define __PLACEMENT_DELETE 1	/* Predefined */
+#define __EDG_RUNTIME_USES_NAMESPACES 1	/* Predefined */
+#define __EDG_IA64_ABI 1	/* Predefined */
+#define __EDG_IA64_ABI_VARIANT_CTORS_AND_DTORS_RETURN_THIS 1	/* Predefined */
+#define __TI_INT40_T__ 1	/* Predefined */
+#define __TI_C99_COMPLEX_ENABLED__ 1	/* Predefined */
+#define __EDG_TYPE_TRAITS_ENABLED 1	/* Predefined */
+#define __edg_front_end__ 1	/* Predefined */
+#define __EDG_VERSION__ 404	/* Predefined */
+#define __EDG_SIZE_TYPE__ unsigned int	/* Predefined */
+#define __EDG_PTRDIFF_TYPE__ int	/* Predefined */
+#define __GNUC_MINOR__ 3	/* Predefined */
+#define __GNUC_PATCHLEVEL__ 0	/* Predefined */
+#define __VERSION__ "EDG g++ 4.3 mode"	/* Predefined */
+#define __TI_COMPILER_VERSION__ 8001007	/* Predefined */
+#define __COMPILER_VERSION__ 8001007	/* Predefined */
+#define _TMS320C6X 1	/* Predefined */
+#define __TMS320C6X__ 1	/* Predefined */
+#define _TMS320C64_PLUS 1	/* Predefined */
+#define _TMS320C6400_PLUS 1	/* Predefined */
+#define _TMS320C6400 1	/* Predefined */
+#define _LITTLE_ENDIAN 1	/* Predefined */
+#define __TI_EABI__ 1	/* Predefined */
+#define __TI_ELFABI__ 1	/* Predefined */
+#define __TI_TLS__ 1	/* Predefined */
+#define __TI_32BIT_LONG__ 1	/* Predefined */
+#define __SIZE_T_TYPE__ unsigned	/* Predefined */
+#define __PTRDIFF_T_TYPE__ int	/* Predefined */
+#define __WCHAR_T_TYPE__ unsigned short	/* Predefined */
+#define __little_endian__ 1	/* Predefined */
+#define __TI_STRICT_ANSI_MODE__ 0	/* Predefined */
+#define __TI_WCHAR_T_BITS__ 16	/* Predefined */
+#define __TI_GNU_ATTRIBUTE_SUPPORT__ 1	/* Predefined */
+#define __TI_STRICT_FP_MODE__ 1	/* Predefined */
