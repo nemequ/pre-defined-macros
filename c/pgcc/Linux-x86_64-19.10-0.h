86dc383288901f907aad82ab07caa0f8a23f2ba1
Evan Nemerson <evan@nemerson.com>
2019-11-15 12:20:37 -0800

    Updates for TI, PGI, and Intel compilers.

---
+++ c/pgcc/Linux-x86_64-19.10-0.h
@@ -0,0 +1,33 @@
+#define unix  1
+#define __unix  1
+#define __unix__  1
+#define linux  1
+#define __linux  1
+#define __linux__  1
+#define __NO_MATH_INLINES  1
+#define __LP64__  1
+#define __x86_64  1
+#define __x86_64__  1
+#define __LONG_MAX__  9223372036854775807L
+#define __SIZE_TYPE__  unsigned long int
+#define __PTRDIFF_TYPE__  long int
+#define __amd64  1
+#define __amd64__  1
+#define __k8  1
+#define __k8__  1
+#define __MMX__  1
+#define __SSE__  1
+#define __SSE2__  1
+#define __SSE3__  1
+#define __SSSE3__  1
+#define __PGLLVM__  1
+#define __extension__  
+#define __PGI  1
+#define __LINE__  0
+#define __FILE__  1
+#define __DATE__  "Nov 15 2019"
+#define __TIME__  "12:19:41"
+#define __STDC_VERSION__  199901L
+#define __PGIC__  19
+#define __PGIC_MINOR__  10
+#define __PGIC_PATCHLEVEL__  0
